6771367f7a4958072c1a2c7ce410340a642331e2
Orlandodlpz <143850773+Orlandodlpz@users.noreply.github.com>
2025-02-03 02:11:22 +0000

    updated bobAccount's balance to assimilate the actions he made

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,21 @@
+import java.text.DecimalFormat;
+public class BankAccount {
 
+    public static void main (String [] alo) {
+        double bobAccount = 2175.37;
+        double bobWD = 302.50;
+        double bobDP = 50.03;
+        bobAccount -= bobWD;
+        bobAccount += bobDP;
+        bobAccount /= 2;
+        bobDP = 20.00;
+        bobAccount += bobDP;
+        bobWD = 1;
+        bobAccount -= bobWD;
+        bobAccount *= 2;
+        bobDP = 1;
+        bobAccount += bobDP;
+        DecimalFormat df = new DecimalFormat("0.00");
+        System.out.println("Bob's new balance is: " + df.format(bobAccount));        
+    }
+}
